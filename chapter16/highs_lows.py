38b33bb7de7729af721b09b68affaa4c983d0036
Meeting Guo (郭玉健) <guoyujian_lc@inspur.com>
2019-04-02 17:03:17 +0800

    完善了high_lows.py的代码

--- chapter16/highs_lows.py
+++ chapter16/highs_lows.py
@@ -1,1 +1,33 @@
 import csv
+import matplotlib.pyplot as plt
+from datetime import  datetime
+
+# 从文件中获取日期，最高气温和最低气温
+filename = 'sitka_weather_2014.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    dates, highs, lows = [], [], []
+    # 当数据出现缺失时，可以用try-except检查错误代码
+    for row in reader:
+        current_date = datetime.strptime(row[0], '%Y-%m-%d')
+        dates.append(current_date)
+        high = int(row[1])
+        highs.append(high)
+        low = int(row[3])
+        lows.append(low)
+    print(highs)
+
+# 根据数据绘制图形
+fig = plt.figure(dpi=128, figsize=(10, 6))
+# 注意这里绘制两条折线的方法
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+# 设置图形的格式
+plt.title("Daily high and low temperatures - 2014", fontsize=24)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()  # 绘制斜的日期标签，以免它们彼此重
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+plt.show()
